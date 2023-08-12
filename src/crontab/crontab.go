1255428bb103f9dac547b3860859c7bb732e8efe
zyouan <1787433600@qq.com>
2023-08-12 23:35:14 +0800

    mysql 连接断开重连

--- src/crontab/crontab.go
+++ src/crontab/crontab.go
@@ -3,12 +3,24 @@ package crontab
 import (
 	"github.com/robfig/cron"
 	"monitor/global"
+	"monitor/initializers"
 	"monitor/src/crontab/job"
 )
 
 func Cron() {
 	cron := cron.New()
 	cron.AddFunc("1 * * * *", func() {
+		err := global.DbMysqlXORM.Ping()
+		if err != nil {
+			global.Log(global.LOG_ERROR, "database ping fail:"+err.Error())
+			//重连
+			global.DbMysqlXORM = initializers.InitMysqlCon()
+			rePingErr := global.DbMysqlXORM.Ping()
+			if rePingErr != nil {
+				global.Log(global.LOG_ERROR, " reping fail"+rePingErr.Error())
+				return
+			}
+		}
 		global.Log(global.LOG_INFO, "mysql monitor start")
 		job.MysqlMonitor()
 		global.Log(global.LOG_INFO, "mysql monitor end")
